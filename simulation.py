e4d55dce21a734145546042174b0a9cf169b1a6a
Luca_LH <lafat001@umn.edu>
2025-06-24 14:04:50 -0500

    iniital commit

---
+++ simulation.py
@@ -0,0 +1,139 @@
+import pygame
+import sys
+import numpy as np
+
+# TODO: change from np.vectorize to just broadcasting
+
+""" Constants """
+WIDTH, HEIGHT = 10, 10
+BG_COLOR = (255, 255, 255)
+LINE_COLOR = (255, 0, 0)
+FPS = 30
+
+
+""" Functions """
+# TODO: put these in a different script later on
+def meters_to_pixels(m):
+    """Converts from meters to pixels to display on screen"""
+    return m * 100
+
+
+def pixels_to_meters(p):
+    """Converts from pixels to meters"""
+    return p / 100
+
+
+def convert_to_tuple_list(lst):
+    """Converts a 2XN array to a list of tuples"""
+    pos_lst = []
+    for i in range(len(lst[0])):
+        pos_lst.append((lst[0][i], lst[1][i]))
+
+    return pos_lst
+
+
+""" Person Agent Class """
+class Agent:
+    def __init__(self, x=2, y=5, radius=0.5, color=(0, 0, 255), N_past=17):
+        self.pos = [x, y]
+        self.radius = radius
+        self.color = color
+        self.N_past = N_past  # default sampling the last two seconds
+        self.dragging = False
+        self.offset = [0, 0]
+        self.past_trajectory = np.array(
+            (np.full(N_past, x), np.full(N_past, y)), dtype=object
+        )
+
+    def draw(self, surface):
+        pygame.draw.circle(
+            surface,
+            self.color,
+            (int(meters_to_pixels(self.pos[0])), int(meters_to_pixels(self.pos[1]))),
+            meters_to_pixels(self.radius),
+        )
+
+        pygame.draw.lines(
+            surface,
+            color=LINE_COLOR,
+            closed=False,
+            points=convert_to_tuple_list(
+                np.vectorize(meters_to_pixels)(self.past_trajectory)
+            ),
+        )
+        
+        for i in self.past_trajectory:
+            pygame.draw.circle(
+                surface,
+                
+            )
+
+    def update(self, x, y):
+        self.past_trajectory[:, :-1] = self.past_trajectory[:, 1:]
+        self.past_trajectory[0][-1] = x
+        self.past_trajectory[1][-1] = y
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = event.pos
+            dx = mouse_x - meters_to_pixels(self.pos[0])
+            dy = mouse_y - meters_to_pixels(self.pos[1])
+            if dx**2 + dy**2 <= meters_to_pixels(self.radius) ** 2:
+                self.dragging = True
+                self.offset = [dx, dy]
+        elif event.type == pygame.MOUSEBUTTONUP:
+            self.dragging = False
+        elif event.type == pygame.MOUSEMOTION:
+            if self.dragging:
+                mouse_x, mouse_y = event.pos
+                self.pos = [
+                    pixels_to_meters(mouse_x - self.offset[0]),
+                    pixels_to_meters(mouse_y - self.offset[1]),
+                ]
+
+
+""" Initializing the game + setup """
+pygame.init()
+screen = pygame.display.set_mode((meters_to_pixels(WIDTH), meters_to_pixels(HEIGHT)))
+pygame.display.set_caption("Trajectory Prediction Visualizer")
+clock = pygame.time.Clock()
+font = pygame.font.SysFont("Arial", 24)
+agent = Agent(x=2, y=5)
+sampling_interval_ms = 8.33 / 1000  # sampling at ~8.33 samples/sec
+last_sample_time = 0
+
+
+""" Main Loop """
+running = True
+while running:
+    screen.fill(BG_COLOR)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        agent.handle_event(event)
+        
+    # sampling positions at 0.12 sec/sample
+    current_time = pygame.time.get_ticks()
+    if current_time - last_sample_time >= sampling_interval_ms:
+        agent.update(agent.pos[0], agent.pos[1])
+        last_sample_time = current_time
+
+    # displaying the median speed of the trajectory on the screen
+    speeds = np.sum((agent.past_trajectory[:, :-1] - agent.past_trajectory[:, 1:])**2, axis=0)**1/2
+    median_speed = np.median(speeds / 0.12)
+    speed_text = f"Median speed: {median_speed:.10f} m/s"
+    text_surface = font.render(speed_text, True, (0, 0, 0))
+    text_rect = text_surface.get_rect(
+        bottomright=(meters_to_pixels(WIDTH) - 20, meters_to_pixels(HEIGHT) - 10)
+    )
+    screen.blit(text_surface, text_rect)
+
+    agent.draw(screen)
+
+    pygame.display.flip()
+    clock.tick(FPS)
+
+pygame.quit()
+sys.exit()
---
+++ utils/data.py
@@ -0,0 +1,196 @@
+import torch
+
+from torch.utils.data import Dataset, DataLoader
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+import random
+
+from tqdm import tqdm
+
+
+class TrajectoryDataset(Dataset):
+
+    def __init__(self, csv_file):
+
+        # Load the CSV data
+
+        csv_data = pd.read_csv(csv_file)
+
+        self.N_past = 10
+
+        self.N_future = 3
+
+        self.person_ids = csv_data["id"].unique()
+
+        self.position_data = {}  # mapping from id to trajectory
+
+        self.velocity_data = {}  # mapping from id to trajectory
+
+        for person_id in tqdm(
+            self.person_ids, total=len(self.person_ids), dynamic_ncols=True
+        ):
+
+            trajdata = csv_data[csv_data["id"] == person_id].sort_values(by="time")
+
+            if len(trajdata) < self.N_past + self.N_future:
+
+                continue
+
+            self.position_data[person_id] = trajdata[["x", "y"]].to_numpy().T  # 2 x N
+
+            self.velocity_data[person_id] = (
+                trajdata[["v_x", "v_y"]].to_numpy().T
+            )  # 2 x N
+
+        self.person_ids = list(
+            self.position_data.keys()
+        )  # remove people with short trajectories
+
+        # TODO: compute length
+
+    def __len__(self):
+
+        # The dataset length is the number of rows in the CSV file
+
+        return 1000
+
+    def __getitem__(self, idx):
+
+        random_person_id = int(random.choice(self.person_ids))
+
+        X = self.position_data[random_person_id]
+
+        V = self.velocity_data[random_person_id]
+
+        random_frame = int(random.randint(self.N_past, X.shape[1] - 1 - self.N_future))
+
+        # Determine the indices for past (10) and future (3) points
+
+        X_past = X[:, random_frame - self.N_past + 1 : random_frame + 1]
+
+        X_future = X[:, random_frame + 1 : random_frame + 1 + self.N_future]
+
+        V_past = V[:, random_frame - self.N_past + 1 : random_frame + 1]
+
+        V_future = V[:, random_frame + 1 : random_frame + 1 + self.N_future]
+
+        # TODO: Data augmentation here
+
+        return X_past, X_future, V_past, V_future
+
+        # # Convert past and future points to tensors
+
+        # past_x = torch.tensor(past_points.iloc[:, 0].values, dtype=torch.float32)
+
+        # past_y = torch.tensor(past_points.iloc[:, 1].values, dtype=torch.float32)
+
+        # future_x = torch.tensor(future_points.iloc[:, 0].values, dtype=torch.float32)
+
+        # future_y = torch.tensor(future_points.iloc[:, 1].values, dtype=torch.float32)
+
+        # # Concatenate past and future to form a trajectory
+
+        # x_trajectory = torch.cat([past_x, future_x])
+
+        # y_trajectory = torch.cat([past_y, future_y])
+
+        # return x_trajectory, y_trajectory
+
+
+def plot_trajectory(x_past, x_future, v_past, v_future):
+
+    # plt.figure(figsize=(8, 6))
+
+    fig, axes = plt.subplots(1, 2, figsize=(16, 6))
+
+    x_past_coords = x_past[0, :].numpy()
+
+    y_past_coords = x_past[1, :].numpy()
+
+    x_future_coords = x_future[0, :].numpy()
+
+    y_future_coords = x_future[1, :].numpy()
+
+    dt = 0.12
+
+    vx_past_coords = v_past[0, :].numpy()
+
+    vy_past_coords = v_past[1, :].numpy()
+
+    vx_future_coords = v_future[0, :].numpy()
+
+    vy_future_coords = v_future[1, :].numpy()
+
+    axes[0].plot(x_past_coords, y_past_coords, marker="o", linestyle="-", color="r")
+
+    axes[0].plot(x_future_coords, y_future_coords, marker="o", linestyle="-", color="b")
+
+    axes[0].set_xlabel("x")
+
+    axes[0].set_ylabel("y")
+
+    axes[0].set_title("Positions")
+
+    axes[1].plot(x_past_coords, y_past_coords, marker="o", linestyle="-", color="k")
+
+    axes[1].plot(x_future_coords, y_future_coords, marker="o", linestyle="-", color="k")
+
+    axes[1].quiver(
+        x_past_coords,
+        y_past_coords,
+        vx_past_coords,
+        vy_past_coords,
+        color="r",
+        angles="xy",
+        scale_units="xy",
+        scale=1 / dt,
+    )
+
+    axes[1].quiver(
+        x_future_coords,
+        y_future_coords,
+        vx_future_coords,
+        vy_future_coords,
+        color="b",
+        angles="xy",
+        scale_units="xy",
+        scale=1 / dt,
+    )
+
+    axes[1].set_title("Velocity Vectors")
+
+    axes[1].set_xlabel("x")
+
+    axes[1].set_ylabel("y")
+
+    axes[1].grid(True)
+
+    plt.show()
+
+
+# Example usage:
+
+csv_file = "entire_scene_crowd_data_sample.csv"  # Path to your CSV file
+
+dataset = TrajectoryDataset(csv_file)
+
+# dataset.__getitem__(300)
+
+
+# Create a DataLoader
+
+dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+
+
+# Iterate through the DataLoader
+
+for x_past, x_future, v_past, v_future in dataloader:
+
+    # Plot the trajectory
+
+    plot_trajectory(x_past[0], x_future[0], v_past[0], v_future[0])
+
+    # break  # Only plot the first batch to avoid unnecessary looping
