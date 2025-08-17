97bf4b3de4c1c76fe6d0acc5b6043df202a3838c
Laurel Sun <86388357+sunlaurel@users.noreply.github.com>
2025-08-17 12:08:47 -0700

    created new sim

--- simulation_rl.py
+++ simulation_rl.py
@@ -1,1 +1,123 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
 import pygame
+import sys
+from tqdm import tqdm
+from aml_rl.network import Policy
+import aml_rl.single_agent_dd_env as env
+
+
+# initializing the screen
+pygame.init()
+screen_width = 500
+screen_height = screen_width
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+dt = 0.1
+v_max = 1.0
+state = env.reset()
+
+
+# setting the size of the arena in meters
+arena_size = 1.8
+agent_radius = 0.11
+goal_radius = 0.2
+
+
+# variables of simulation
+RESET = True
+RUNNING = True
+CURSOR_XY = (screen_width / 2, screen_height / 2)  # cursor position is in pixels
+
+
+state_size, action_size = env.init(
+    arena_size, agent_radius, goal_radius / 2, goal_radius, [0.7, 2.0]
+)
+
+
+# initializing the policy network
+save_path = "./weights/best-weights-rl/policy_latest.pt"
+policy = Policy(state_size, action_size)
+policy.load_state_dict(
+    torch.load(save_path, map_location=torch.device("cpu"))
+)  # use this for non-gpu machines
+
+
+# renders one env frame
+def render(screen, state):
+    # breakpoint()
+    env.render(state, screen, screen_width, screen_height)
+
+
+def pixels_to_meters(pixel):
+    return pixel * arena_size / screen_width
+
+
+def meters_to_pixels(meter):
+    return meter * screen_width / arena_size
+
+
+def set_goal(state, new_goal):
+    state[-2] = new_goal[0]
+    state[-1] = new_goal[1]
+    return state
+
+
+if __name__ == "__main__":
+    while RUNNING:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                RUNNING = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    RESET = True
+                elif event.key == pygame.K_q:
+                    RUNNING = False
+                elif event.key == pygame.K_m:
+                    CURSOR_XY = (screen_width / 2, screen_height / 2)
+
+        if RESET:
+            env.reset()
+            RESET = False
+
+        # TODO: set the env goal here
+        state = set_goal(state, np.vectorize(pixels_to_meters)(CURSOR_XY))
+
+        action_dist = policy(
+            torch.tensor(env.sense(state), dtype=torch.float32)
+        )  # policy returns action distribution
+        action = (
+            action_dist.sample().cpu().detach().numpy() * v_max
+        )  # samples from the action distribution
+
+        # breakpoint()
+
+        new_state, _, _ = env.step(state, action, dt)
+        render(screen, new_state)
+
+        pygame.display.flip()
+        clock.tick(15)
+
+    pygame.quit()
+    sys.exit()
+
+# RESET = True  # make a key callback so 'r' sets to true
+# RUNNING = True  # make a key callback so 'q' sets to false
+# CURSOR_XY = (x, y)  # have some event set this too
+# pygame.init()
+# screen = ...  # fill in
+
+# while RUNNING:
+#     # check pygame keys here
+#     if RESET:
+#         env.reset()
+#         RESET = False
+#     env.set_goal(CURSOR_XY)
+#     state = (
+#         env.get_observation()
+#     )  # state was updated by the cursor, so you need to get it explicitly
+#     action = agent(state)
+#     new_state = env.step()
+#     render(screen, new_state)
--- utils/models.py
+++ utils/models.py
@@ -1,7 +1,5 @@
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.distributions import Independent
-from torch.distributions import Normal
 import torch
 import json
 
@@ -13,48 +11,6 @@ import json
 ##  - also contains some baseline models (standing    ##
 ##    still and maintaining velocity)                 ##
 ########################################################
-
-# Policy network - Predicts actions distribution from the current state
-policy_size = 32
-value_size = 128
-
-
-class Policy(nn.Module):
-    def __init__(self, state_size, action_size):
-        super(Policy, self).__init__()
-        self.fc1 = nn.Linear(state_size, policy_size)
-        self.fc2 = nn.Linear(policy_size, policy_size)
-        self.fc3 = nn.Linear(policy_size, action_size)
-        self.std_dev_v = nn.Parameter(
-            torch.ones(action_size)
-        )  # Trainable standard deviation (will be softplus transformed to ensure positivity)
-
-    # Returns action distribution
-    def forward(self, x):
-        x = torch.tanh(self.fc1(x))
-        x = torch.tanh(self.fc2(x))
-        x = self.fc3(x)
-        x = torch.tanh(x)
-        action_mean = x
-        action_distrib = Independent(Normal(action_mean, F.softplus(self.std_dev_v)), 1)
-        return action_distrib
-
-
-# Value network - Predicts state value from the current state
-class Value(nn.Module):
-    def __init__(self, state_size):
-        super(Value, self).__init__()
-        self.fc1 = nn.Linear(state_size, value_size)
-        self.fc2 = nn.Linear(value_size, value_size)
-        self.fc3 = nn.Linear(value_size, 1)
-
-    def forward(self, x):
-        x = torch.tanh(self.fc1(x))
-        x = torch.tanh(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-
 """ Multi Layer Perceptron (MLP) for predicting N steps into the future """
 class MultiLayer(nn.Module):
 
